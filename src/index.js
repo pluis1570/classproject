138e29bc2418b66513bc04b511128680860ccd44
pluis1570 <laur.luyp@hotmail.com>
2018-09-10 09:50:22 +0200

    adding index

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ require ('./index.css')
 tabletop.init({
   key: 'https://docs.google.com/spreadsheets/d/19c5VxyfV5P3-4jUwramk5fiyFLm4IPGqqLqo8_6f7-o/pubhtml',
 callback: function (tabletopData, tabletop) {
-  var renderPlaces = render(tabletopData)
+  var renderedPlaces = render(tabletopData)
 
   document.body.appendChild(render(tabletopData))
 },
